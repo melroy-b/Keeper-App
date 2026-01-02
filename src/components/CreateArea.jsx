6617a3a4aa01437a5af8f65ef7bbf2a71a338c1d
Melroy Barreto <melroybarreto61@gmail.com>
2026-01-02 13:34:09 +0530

    Responsive UI, class update in style.css

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
 
 function CreateArea(props) {
   const [noteItem, setNoteItem] = useState({
     title: "",
     content: "",
   });
+
+  const [isTyping, setIsTyping] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -19,27 +24,39 @@ function CreateArea(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    props.onAdd(noteItem);
+    if (noteItem.title !== "" && noteItem.content !== "") props.onAdd(noteItem);
     setNoteItem({ title: "", content: "" });
+  }
+
+  function handleClick() {
+    setIsTyping(true);
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form className="create-note" onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
           name="title"
           placeholder="Title"
           value={noteItem.title}
+          hidden={!isTyping}
         />
+
         <textarea
+          onMouseDown={handleClick}
           onChange={handleChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isTyping ? "3" : "1"}
           value={noteItem.content}
         />
-        <button type="submit">Add</button>
+
+        <Zoom in={isTyping}>
+          <Fab type="submit">
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
